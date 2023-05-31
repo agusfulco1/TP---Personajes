e4cd0e17ca3c87c2e0336cf485facabd76eff078
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-05-31 09:04:25 -0300

    delete terminado

--- index.js
+++ index.js
@@ -7,7 +7,25 @@ app.get('/characters', async (req,res) => {
     const personajes = await personajesService.getAll()
     res.status(200).send(personajes)
 })
-
+app.delete('/characters/:id', async (req,res) => {
+    try {
+        console.log(req.params.id)
+        await personajesService.deleteByID(req.params.id)
+        res.status(200).json({ message: 'Personaje borrada'})
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: 'Fallo el delete'})
+    }
+})
+app.post('/characters',async (req, res) => {    
+    try {
+        await personajesService.insert(req.body)
+        res.status(200).json({ message: 'Personaje creada' })
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({error: 'Fallo el insert' })
+    }
+})
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
--- src/services/personajes-services.js
+++ src/services/personajes-services.js
@@ -1,17 +1,43 @@
 import { config } from "../../dbconfig.js"
 import sql from "mssql"
-const {MAX, NVarChar, VARCHAR} = sql
 
 export class personajesService {
     static getAll = async() => {
         try {
             let pool = await sql.connect(config);
             let result = await pool.request()
-                                .query('SELECT Id,Titulo,Imagen FROM PeliculaSerie')
+                                .query('SELECT Id,Nombre,Imagen FROM Personaje')
             return result.recordsets[0];
         }
         catch (error) {
             console.log(error)
+        }
+    }
+    static deleteByID = async(id) => {
+        try {
+            let pool = await sql.connect(config);
+            let result = await pool.request()
+                            .input('pId', sql.Int, id)
+                            .query('DELETE FROM Personaje WHERE Id = @pId')
+        } 
+        catch (error) {
+            console.log(error);
+        }   
+    }
+    static insert = async(personaje) => {
+        try {
+            let pool = await sql.connect(config);
+            let result = await pool.request()
+                                .input('pImagen', personaje.Imagen)
+                                .input('pNombre', personaje.Nombre)
+                                .input('pEdad', personaje.Edad)
+                                .input('pPeso', personaje.Peso)
+                                .input('pHistoria', personaje.Historia)
+                                .query('INSERT INTO Pizzas (Imagen, Nombre, Edad, Peso, Historia) VALUES (@pImagen, @pNombre, @pEdad, @pPeso, @pHistoria)')
+            
+        }
+        catch (error) {
+            console.log(error);
         }
     }
 }
