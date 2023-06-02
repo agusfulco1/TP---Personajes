1e3a1e2333097e29ae39249510cbe33eee739ec0
XaXhade <you@example.com>
2023-06-01 23:08:06 -0300

    insert y getByID hechos y todo lo demas verificado

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import express from "express"
 import personajesService from "./src/services/personajes-services.js";
 const app = express();
 const port = 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 app.get('/characters', async (req,res) => {
     const personajes = await personajesService.getAll()
@@ -9,9 +12,8 @@ app.get('/characters', async (req,res) => {
 })
 app.delete('/characters/:id', async (req,res) => {
     try {
-        console.log(req.params.id)
         await personajesService.deleteByID(req.params.id)
-        res.status(200).json({ message: 'Personaje borrada'})
+        res.status(200).json({ message: 'Personaje borrado'})
     } catch (error) {
         console.log(error);
         res.status(500).json({ message: 'Fallo el delete'})
@@ -20,11 +22,28 @@ app.delete('/characters/:id', async (req,res) => {
 app.post('/characters',async (req, res) => {    
     try {
         await personajesService.insert(req.body)
-        res.status(200).json({ message: 'Personaje creada' })
+        res.status(200).json({ message: 'Personaje creado' })
     } catch (error) {
         console.error(error);
         res.status(500).json({error: 'Fallo el insert' })
     }
+})
+app.put('/characters', async (req, res) => {
+    try {
+        await personajesService.update(req.body)
+        res.status(200).json({ message: 'Personaje actualizado'})
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({error: 'Fallo el insert'})
+    }
+})
+app.get('/characters/:id', async (req, res) => {
+    const pizza = await personajesService.getById(req.params.id)
+    if (!pizza) {
+        res.status(200).send("No existe esa pizza")
+    }
+    res.status(200).send(pizza)
+    
 })
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
--- src/services/personajes-services.js
+++ src/services/personajes-services.js
@@ -26,6 +26,7 @@ export class personajesService {
     }
     static insert = async(personaje) => {
         try {
+            console.log(personaje)
             let pool = await sql.connect(config);
             let result = await pool.request()
                                 .input('pImagen', personaje.Imagen)
@@ -33,11 +34,40 @@ export class personajesService {
                                 .input('pEdad', personaje.Edad)
                                 .input('pPeso', personaje.Peso)
                                 .input('pHistoria', personaje.Historia)
-                                .query('INSERT INTO Pizzas (Imagen, Nombre, Edad, Peso, Historia) VALUES (@pImagen, @pNombre, @pEdad, @pPeso, @pHistoria)')
+                                .query('INSERT INTO Personaje (Imagen, Nombre, Edad, Peso, Historia) VALUES (@pImagen, @pNombre, @pEdad, @pPeso, @pHistoria)')
             
         }
         catch (error) {
             console.log(error);
+        }
+    }
+    static update = async(personaje) => {
+        try {
+            console.log(personaje)
+            let pool = await sql.connect(config);
+            let result = await pool.request()
+                                .input('pId', personaje.Id)
+                                .input('pImagen', personaje.Imagen)
+                                .input('pNombre', personaje.Nombre)
+                                .input('pEdad', personaje.Edad)
+                                .input('pPeso', personaje.Peso)
+                                .input('pHistoria', personaje.Historia)
+                                .query('UPDATE Personaje SET Imagen=@pImagen, Nombre=@pNombre, Edad=@pEdad, Peso=@pPeso, Historia=@pHistoria WHERE Id = @pId')
+            console.log(result)
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
+    static getByID = async() => {
+        try {
+            let pool = await sql.connect(config);
+            let result = await pool.request()
+                                .query('SELECT P.*, Titulo FROM Personaje P INNER JOIN PeliculaxPersonaje PP ON PP.idPersonaje = P.Id INNER JOIN PeliculaSerie PS ON PS.Id = PP.idPelicula')
+            return result.recordsets[0];
+        }
+        catch (error) {
+            console.log(error)
         }
     }
 }
